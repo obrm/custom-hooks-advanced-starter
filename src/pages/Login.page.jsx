7c7bfe6d9009fe4924df6baaeae3016f9cc8fefd
Ori Baram <obrm770@gmail.com>
2023-09-18 13:31:38 +0300

    first commit

---
+++ src/pages/Login.page.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+import { Logo, FormRow } from '../components';
+import Wrapper from '../styles/styled/Login.styled';
+
+const Login = () => {
+  return (
+    <Wrapper className='full-page'>
+      <form className='form'>
+        <Logo />
+        {/* name field */}
+        
+        {/* email field */}
+        
+        {/* password field */}
+        <button type='submit' className='btn btn-block'>
+        {/* Loading */}
+        </button>
+        <Link to='/' className='btn btn-block btn-light mt'>
+          Back
+        </Link>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Login;
